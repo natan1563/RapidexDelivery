07a33ce624bd5596b2b2454c51af23df7d9d8b22
natan1563 <natromafe@gmail.com>
2021-02-05 14:14:17 -0300

    Alterações visuais no template

--- App/template/footer.php
+++ App/template/footer.php
@@ -1,19 +1,20 @@
-<footer class="mt-3 container bg-dark mw-100">
-    <div class="row">
+<footer class="mt-5 mb-0 container bg-dark mw-100 fixed-bottom">
+    <div class="row align-self-center">
 
-        <div class="col-3 text-light mt-2">
+        <div class="col-3 text-light mt-2 ">
             <u class="font-weight-bold h5">Dados da empresa</u>
             <p class="mt-2">Telefone para contato: (71) 99266-4106</p>
             <p class="">Email: natromafe@gmail.com</p>
         </div>
 
-        <div class="col-5 text-light">
+        <div class="col-5 text-light mt-2">
             <u class="font-weight-bold h5">Dados da empresa</u>
             <p class="mt-2">Telefone para contato: (71) 99266-4106</p>
             <p class="">Email: natromafe@gmail.com</p>
         </div>
 
     </div>
+    <p class="text-center text-light">&#169; Todos direitos reservados a Natã Romão Ferreira</p>
 </footer>
 
 </html>
--- Public/index.php
+++ Public/index.php
@@ -17,23 +17,17 @@ $resources = Products::listProducts();
     <!-- Menu -->
     <?php require_once '../App/template/header.php'; ?>
 
-    <div class="mt-3 w-75 py-4 border border-danger rounded mx-auto mt-1 bg-danger container">
+    <div class="mt-5 w-75 py-4 border border-danger rounded mx-auto mt-1 bg-danger container">
         <div class="row row-cols-1 row-cols-md-2 g-4">
 
-        <?php foreach($resources as $resource):  $ingredients = explode(',', $resource->data->ingredients[0]); ?>
+        <?php foreach($resources as $resource): ?>
             <div class="col">
                 <div class="card mt-2">
                     
                     <div class="card-body">
                         <h5 class="card-title"><?=$resource->data->name?></h5>
                         <p class="card-text"><?=$resource->data->description?></p>
-                        <ul>
-                        <?php foreach($ingredients as $ingredient): ?>
-                            
-                            <li><?=$ingredient?></li>
-
-                        <?php endforeach?>
-                        </ul>
+                        <p class="card-text">Ingredientes: <?=$resource->data->ingredients[0]?></p>
                     </div>
                 </div>
             </div>
